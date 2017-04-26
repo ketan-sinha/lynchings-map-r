1b47919fc3a76923264f5fb221cc69fd2db44ad9
Ketan Sinha <ksinha2@uh.edu>
2017-04-26 01:59:53 -0500

    Added HTML markup for popups

--- leaflet.R
+++ leaflet.R
@@ -1,9 +1,20 @@
-
+library(tidyverse)
+library(leaflet)
+library(rgdal)
 
 lynchings <- readOGR(dsn = "db/lynchings.geojson", layer = "OGRGeoJSON")
 
 pal <- colorNumeric("viridis", NULL)
 
+popup_content <- paste(sep="",
+                       "<b>",lynchings$Name,"</b>, <i>",lynchings$Gender,"</i></br>",
+                       lynchings$Year,"/",lynchings$Month,"/",lynchings$Day,"</br>",
+                       "<i>",lynchings$Race,"</i></br>",
+                       lynchings$Accusation,"</br>",
+                       "Death: <i>",lynchings$Method.of.Death, "</i>")
+
 leaflet() %>%
   addTiles() %>%
-  addMarkers(data = lynchings, clusterOptions = markerClusterOptions())
+  addMarkers(data = lynchings, 
+             clusterOptions = markerClusterOptions(), 
+             popup = popup_content)
